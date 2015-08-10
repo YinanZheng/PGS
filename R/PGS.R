b920d2ec7377ea0146583aba065841377a8535ba
Yinan Zheng <y-zheng@northwestern.edu>
2015-08-10 00:31:05 -0500

    Modified DESCRIPTION, PGS.R, pheno.rda. Added prGEE.R, prLMM.R

--- R/PGS.R
+++ R/PGS.R
@@ -1,36 +1,43 @@
 PGS<-function(
-              y.vect,          #### Dependent variable vector
-              id.vect,         #### Subjuect id variable to indicate repeated measures
-              M,               #### Biomarkers matrix (each row represents samples, each column represents biomarkers/probes)
-              COV,             #### Covariates matrix
-              PS,	             #### Prescreening results. Should have been properly ranked. The first column must be the biomarkers name and must be the same as the column names of biomarkers matrix)
-              Pm.vect,         #### Pm vector, a vector of the number of top ranking biomarkers 	       
-              lam.vect,        #### lambda vector, a vector of PGEE penalty parameter
-              n,               #### Sample size
-              m,               #### Number of repeated measures
-              rho=0.3,         ####
-              fold=10,         #### 10-fold cross-validation for calculating grid error
-              sigcut=10^-3,    #### If PGS estimate of biomarker is greater than 0.001, then the biomarker is selected 
-              eps=10^-10,      ####
-              eps.stop=0,      ####
-              max.step=50,     ####
-              corr_str="ar1",  #### Specify the correlation structure
-              parallel = TRUE, #### parallel computing model
-              seed = NULL      #### seed
+              y.vect,            #### Dependent variable vector
+              id.vect,           #### Subjuect id variable to indicate repeated measures
+              M,                 #### Genomic data (each row represents samples, each column represents genomic marks)
+              COV = NULL,        #### Covariates data
+              PS,	               #### Prescreening results. Should have been properly ranked. The first column must be the biomarkers name and must be the same as the column names of biomarkers matrix)
+              Pm.vect,           #### Pm vector, a vector of the number of top ranking biomarkers 	       
+              lam.vect,          #### lambda vector, a vector of PGEE penalty parameter
+              n,                 #### Sample size
+              m,                 #### Number of repeated measures
+              rho = 0.3,         ####
+              fold = 10,         #### 10-fold cross-validation for calculating grid error
+              sigcut = 10^-3,    #### If PGS estimate of biomarker is greater than 0.001, then the biomarker is selected 
+              eps = 10^-10,      ####
+              eps.stop = 0,      ####
+              max.step = 50,     ####
+              corr_str = "ar1",  #### Specify the correlation structure
+              parallel = TRUE,   #### parallel computing model
+              seed = NULL        #### seed
              )
 {
+  if (is.null(seed)) seed = as.integer(Sys.time())  # Use current time as seed if not specified
   
-  if (is.null(seed)) seed = as.integer(Sys.time())
+  if (!is.null(COV))
+  {COV <- as.matrix((model.matrix(~.,COV))[,-1])}
   
-  Mnames=colnames(M)     # Extract miRNA names
+  if(is.null( (PS.Morder = as.character(rownames(PS))) ))
+  {stop("The row names of the pre-ranking results must be the name of genomic marks.")}
+  
+  M <- as.matrix(M)
+  
+  M.names=colnames(M)     # Extract genomic marks names
   conf.names=colnames(COV)   # Extract confounder names
-  PS.Morder = as.character(PS[,1]) # Extract genomic marks by prescreening order
-  M.ps<-M[,match(PS.Morder,Mnames)]   # Sort the genomic data based on the prescreening order
+  
+  M.ps<-M[,match(PS.Morder,M.names)]   # Sort the genomic data based on the prescreening order
   
   L.Pm = length(Pm.vect)
   L.lam = length(lam.vect)
   
-  ### Initialize the rooms for results
+  ### Initialize rooms to store results
   grid.err = matrix(nrow = L.Pm, ncol = L.lam)   # Initiate error grid, column = lambda, row = Pm
   lam.sel = rep(NA,L.Pm)  # Initiate optimized lambda for each Pm
   beta.shrink.corr.list = vector("list", L.Pm)   # Initiate penalized Beta (coefficient) estimates for each Pm
@@ -45,8 +52,9 @@ PGS<-function(
     res_seq <- vector("list", L.Pm) 
     for (k in 1:L.Pm)
     {
+      cat(paste0("Start running PGS with single core...   (",Sys.time(),")\n"))
       pm<-Pm.vect[k]
-      print(paste("Running Top",pm,"biomarkers..."))
+      print(paste("Running Top",pm,"marks..."))
       x.mat<-as.matrix(cbind(M.ps[,1:pm],COV))   # Attach the confounders with the top markers
       set.seed(seed)
       res_seq[k] = list(one_run_grid_cpp(y.vect, x.mat, id.vect, n, m, ncol(x.mat), fold, lam.vect, rho, eps, eps.stop, max.step, corr_str))
@@ -55,9 +63,9 @@ PGS<-function(
   
   if (parallel == TRUE)
   {
-    require(doParallel)
-    res_par <- vector("list", L.Pm) 
+    cat(paste0("Start running PGS with ",detectCores()," cores in parallel...   (",Sys.time(),")\n"))
     registerDoParallel(cores = detectCores())
+    res_par <- vector("list", L.Pm) 
     res_par <- foreach(k = 1:length(Pm.vect), .combine = c, .packages=c("PGS")) %dopar%  
     {        
       pm<-Pm.vect[k]
@@ -83,12 +91,8 @@ PGS<-function(
   rownames(grid.err) <- Pm.vect  
   colnames(grid.err) <- lam.vect
   
-  # write.csv(grid.err,"Grid.csv")    # Save the CV-error grid
-  # cat(">>> Cross-validation error grid saved\n")
-  
   best.ind<-which(grid.err==min(grid.err),arr.ind=T)
-  print(paste0("The best selection results achieved using top ",Pm.vect[best.ind[1]]," biomarkers with lambda = ",lam.vect[best.ind[2]]))
-  
+
   beta<-beta.shrink.corr.list[[ best.ind[1] ]]
   varb<-var.sand.corr.list[[ best.ind[1] ]]
   se<-sqrt(varb)
@@ -112,5 +116,8 @@ PGS<-function(
              est.sigma2.corr = est.sigma2.corr.vect[best.ind[1]],
              Pm.vect = Pm.vect,
              lam.vect = lam.vect)
+  cat(paste0("Done!    (",Sys.time(),")"))
+  
+  cat(paste0(">>> The best selection results achieved using top ",Pm.vect[best.ind[1]]," biomarkers with lambda = ",lam.vect[best.ind[2]]),"\n")
   return(res)
 }
---
+++ R/prGEE.R
@@ -0,0 +1,54 @@
+## Pre-ranking genomic marks using generalized estimation equation (GEE)
+
+prGEE <- function(y.vect, id.vect, M, COV=NULL, parallel=TRUE, write=FALSE)
+{
+  if (parallel == TRUE)
+  {  
+    num_cores <- detectCores()
+    cat(paste0("Running pre-ranking with ", num_cores, " cores in parallel.\n"))
+    options(mc.cores = num_cores)
+  } else {
+    num_cores <- 1
+    cat(paste0("Running pre-ranking with single core.\n"))
+    options(mc.cores = num_cores)
+  }
+  
+  if (is.null(COV))
+  {
+    datarun = data.frame(id.vect = id.vect, y.vect=y.vect, Mone = NA)
+    modelstatement = y.vect ~ Mone
+  } else {
+    COV <- data.frame(COV)
+    COV <- data.frame(model.matrix(~.,COV))[,-1]
+    conf.names = colnames(COV)          
+    datarun = data.frame(id.vect = id.vect, y.vect=y.vect, Mone = NA, COV = COV)
+    modelstatement = eval(parse(text=(paste0("y.vect ~ Mone +", paste0(paste0("COV.",conf.names),collapse = "+")))))
+  }
+  
+  doOne <- function(i, datarun){
+    datarun$Mone <- M[,i]
+    model <- try(geeglm(modelstatement, data = datarun, id = id.vect))
+    if(class(model) == "try-error"){
+      b <- rep(NA, 4)
+    } else {
+      res=summary(model)$coefficients
+      b <- as.numeric(res[2,])
+    }
+    invisible(b)
+  }
+  
+  mirnames = colnames(M) 
+  
+  results <- data.frame(do.call(rbind, mclapply( setNames(seq_len(ncol(M)), colnames(M)), doOne, datarun = datarun))) 
+  results = setNames(results, c("Estimate","SE","Wald","pvalue"))
+  results = results[order(results$pvalue),]
+  results$BH.FDR=p.adjust(results$pvalue,"fdr")              # calculate Benjamini and Hochberg FDR
+  results$Bonferroni=p.adjust(results$pvalue,"bonferroni")   # calculate Bonferroni adjusted p-value
+  
+  if (write == TRUE)
+  {
+    write.csv(ps_lmm,"Pre-ranking_GMM.csv")
+    cat(paste0("Pre-ranking results using GMM has been saved to ",getwd(),".\n"))
+  }
+  return(results)
+}
