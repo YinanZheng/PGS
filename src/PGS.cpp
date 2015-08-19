40a26d710e4065f3140da620d1ca2cfd00382571
YinanZheng <y-zheng@northwestern.edu>
2015-08-19 01:20:14 -0500

    PGS_0.0.4
    Optimization and enhancement of PGS core algorithm

--- src/PGS.cpp
+++ src/PGS.cpp
@@ -16,26 +16,28 @@ vec q_scad_cpp(vec theta, double lambda, double a) {
 }
 
 // [[Rcpp::export]]
-List S_H_E_normal_cpp(vec y_vect, mat x_mat, vec id_vect, mat hat_R, vec beta_val, int n, int m, double lam, double eps){
+List S_H_E_normal_cpp(vec y_vect, mat x_mat, vec id_vect, mat hat_R, vec beta_val, int n, vec m, double lam){
   int p = x_mat.n_cols;
+  int nm = x_mat.n_rows;
+  vec m_cum = cumsum(m);
   double sigma = 0;
   vec beta_val_abs = abs(beta_val), Y;
   mat resi, S(p,1), H(p,p), E, X, Xh;
   S.fill(0.0); H.fill(0.0);
   
   resi = y_vect - x_mat * beta_val;
-  sigma = as<double>(wrap(sqrt(sum(pow(resi, 2))/(n*m - p))));
+  sigma = as<double>(wrap(sqrt(sum(pow(resi, 2))/(nm - p))));
   
   for(int i=0; i<n; i++)
   {
-    X = x_mat.rows((i*m),(i*m+1));
-    Y = y_vect.subvec((i*m),(i*m+1));
+    X = x_mat.rows((m_cum(i) - (m(i)-1)), m_cum(i));
+    Y = y_vect.subvec((m_cum(i) - (m(i)-1)), m_cum(i));
     Xh = X.t() * hat_R.i();
     S = S + pow(sigma,-2) * (Xh * (Y - X * beta_val));
     H = H + pow(sigma,-2) * (Xh * X);
   }
   
-  E = diagmat(q_scad_cpp(beta_val_abs, lam, 3.7) / (beta_val_abs + eps));
+  E = diagmat(q_scad_cpp(beta_val_abs, lam, 3.7) / (beta_val_abs + 1e-6));
   
   return List::create(Named("S") = S,
                       Named("H") = H,
@@ -45,23 +47,24 @@ List S_H_E_normal_cpp(vec y_vect, mat x_mat, vec id_vect, mat hat_R, vec beta_va
 }
 
 // [[Rcpp::export]]
-List beta_shrink_normal_cpp(vec y_vect, mat x_mat, vec id_vect, mat hat_R, vec beta_ini, int n, int m, double lam, double eps, int eps_stop, int max_step){
+List beta_shrink_normal_cpp(vec y_vect, mat x_mat, vec id_vect, mat hat_R, vec beta_ini, int n, vec m, double lam, int eps_stop){
   int p = x_mat.n_cols;
-  vec beta_new = beta_ini, Y, var_sand;
   int step_up = 0;
+  vec beta_new = beta_ini, Y, var_sand;
+  vec m_cum = cumsum(m);
   mat S, H, E, M(p,p), X, EPS, hee, hnee;
   M.fill(0.0);
   double flag_stop, sigma;
   
-  List S_H_E_val = S_H_E_normal_cpp(y_vect, x_mat, id_vect, hat_R, beta_new, n, m, lam, eps);
+  List S_H_E_val = S_H_E_normal_cpp(y_vect, x_mat, id_vect, hat_R, beta_new, n, m_cum, lam);
   S = as<mat>(S_H_E_val[0]);
   H = as<mat>(S_H_E_val[1]);
   E = as<mat>(S_H_E_val[2]);
-  while(step_up < max_step)
+  while(flag_stop < eps_stop)
   {
     vec beta_old = beta_new;
     beta_new = beta_old + (H + n * E + (1e-6) * eye(p,p)).i() * (S - n * E * beta_old);
-    S_H_E_val = S_H_E_normal_cpp(y_vect, x_mat, id_vect, hat_R, beta_new, n, m, lam, eps);
+    S_H_E_val = S_H_E_normal_cpp(y_vect, x_mat, id_vect, hat_R, beta_new, n, m, lam);
     S = as<mat>(S_H_E_val[0]);
     H = as<mat>(S_H_E_val[1]);
     E = as<mat>(S_H_E_val[2]);
@@ -71,12 +74,13 @@ List beta_shrink_normal_cpp(vec y_vect, mat x_mat, vec id_vect, mat hat_R, vec b
   
   hat_R = as<mat>(S_H_E_val[3]);
   sigma = as<double>(S_H_E_val[4]);
-  hee = (hat_R + (1e-6) * eye(m,m)).i();
+  
   
   for(int i=0; i<n; i++)
   {
-    X = x_mat.rows((i*m),(i*m+1));
-    Y = y_vect.subvec((i*m),(i*m+1));
+    X = x_mat.rows((m_cum(i) - (m(i)-1)), m_cum(i));
+    Y = y_vect.subvec((m_cum(i) - (m(i)-1)), m_cum(i));
+    hee = (hat_R + (1e-6) * eye(m(i),m(i))).i();
     EPS = Y - X * beta_new;
     M = M + pow(sigma, -4) * ( X.t() * hee * (EPS * EPS.t()) * hee * X );
   }
@@ -94,7 +98,7 @@ List beta_shrink_normal_cpp(vec y_vect, mat x_mat, vec id_vect, mat hat_R, vec b
 
 
 // [[Rcpp::export]]
-List CV_lam_grid_cpp(vec y_vect, mat x_mat, vec id_vect, mat hat_R, vec beta_ini, int n, int m, int fold, vec lam_vect, double eps, int eps_stop, int max_step){
+List CV_lam_grid_cpp(vec y_vect, mat x_mat, vec id_vect, mat hat_R, vec beta_ini, int n, vec m, int fold, vec lam_vect, int eps_stop){
   int lam_length = lam_vect.n_elem;
   int n_train;
   double lam_temp, cv_value, cv_min = math::inf(), lam_min = -1;
@@ -130,7 +134,7 @@ List CV_lam_grid_cpp(vec y_vect, mat x_mat, vec id_vect, mat hat_R, vec beta_ini
           x_test = x_mat.rows(idx_test);
           id_test = id_vect.elem(idx_test);
           
-          beta_train = as<vec>(beta_shrink_normal_cpp(y_train, x_train, id_train, hat_R, beta_ini, n_train, m, lam_temp, eps, eps_stop, max_step)[0]);
+          beta_train = as<vec>(beta_shrink_normal_cpp(y_train, x_train, id_train, hat_R, beta_ini, n_train, m, lam_temp, eps_stop)[0]);
           cv_value += sum(pow((y_test - x_test * beta_train),2));                        
       }
       cv_vect(j) = cv_value;
@@ -147,11 +151,11 @@ List CV_lam_grid_cpp(vec y_vect, mat x_mat, vec id_vect, mat hat_R, vec beta_ini
 }
 
 // [[Rcpp::export]]
-List est_pgee_grid_cpp(vec y_vect, mat x_mat, vec id_vect, mat hat_R, vec beta_ini, int n, int m, int fold, vec lam_vect, double eps, int eps_stop, int max_step){
+List est_pgee_grid_cpp(vec y_vect, mat x_mat, vec id_vect, mat hat_R, vec beta_ini, int n, vec m, int fold, vec lam_vect, double eps, int eps_stop, int max_step){
   List lam_cv = CV_lam_grid_cpp(y_vect, x_mat, id_vect, hat_R, beta_ini, n, m, fold, lam_vect, eps, eps_stop, max_step);
   double lam_sel = as<double>(lam_cv[2]);
   // Run the model using all data with the best lambda
-  List beta_fit = beta_shrink_normal_cpp(y_vect, x_mat, id_vect, hat_R, beta_ini, n, m, lam_sel, eps, eps_stop, max_step);
+  List beta_fit = beta_shrink_normal_cpp(y_vect, x_mat, id_vect, hat_R, beta_ini, n, m, lam_sel, eps_stop);
   return List::create(
                       Named("lam.cv.vector") = as<vec>(lam_cv[1]),
                       Named("beta.shrink") = as<vec>(beta_fit[0]),
@@ -164,37 +168,39 @@ List est_pgee_grid_cpp(vec y_vect, mat x_mat, vec id_vect, mat hat_R, vec beta_i
 
 // [[Rcpp::export]]
 
-mat corr_est_normal_cpp(vec y_vect, mat x_mat, vec id_vect, vec beta_val, int n, int m, std::string corr_str){
+mat corr_est_normal_cpp(vec y_vect, mat x_mat, vec beta_val, int n, std::string corr_str){
   int p = x_mat.n_cols;
-  vec resi = y_vect - x_mat * beta_val, temp_resi, ones(m), alphas(m); ones.fill(1);
-  double sigma = as<double>(wrap(sqrt(sum(pow(resi, 2))/(n * m - p))));
-  mat hat_R(m,m), resi_mat, hat_l(m,m), hat_r(m,m); hat_R.fill(0); hat_r.fill(0);
+  int nm = x_mat.n_rols;
+  vec resi = y_vect - x_mat * beta_val;
+  // vec temp_resi, ones(m), alphas(m); ones.fill(1);
+  double sigma = as<double>(wrap(sqrt(sum(pow(resi, 2))/(nm - p))));
+  // mat hat_R(m,m), resi_mat, hat_l(m,m), hat_r(m,m); hat_R.fill(0); hat_r.fill(0);
   double SUM, SUM_i, alpha;
   resi = resi / sigma;
   
-  if(corr_str == "indep")
-  {
-    hat_R = eye(m,m);
-  }
-  
-  if(corr_str == "exch")
-  {
-    SUM = 0;
-    for (int i=0; i<n; i++)
-    {
-      temp_resi = resi.subvec((i*m),(i*m+1));
-      SUM_i = 0;
-      for (int j=0; j<m; j++)
-      {
-        SUM_i = SUM_i + (sum(temp_resi(j)*temp_resi) - pow(temp_resi(j),2));
-      }
-      SUM = SUM + SUM_i / (m * (m-1));
-    }
-    alpha = SUM / n;
-    alphas.fill(1-alpha);
-    hat_r.diag() = alphas;
-    hat_R = hat_l.fill(alpha) + hat_r;
-  }
+//   if(corr_str == "indep")
+//   {
+//     hat_R = eye(m,m);
+//   }
+//   
+//   if(corr_str == "exch")
+//   {
+//     SUM = 0;
+//     for (int i=0; i<n; i++)
+//     {
+//       temp_resi = resi.subvec((i*m),(i*m+1));
+//       SUM_i = 0;
+//       for (int j=0; j<m; j++)
+//       {
+//         SUM_i = SUM_i + (sum(temp_resi(j)*temp_resi) - pow(temp_resi(j),2));
+//       }
+//       SUM = SUM + SUM_i / (m * (m-1));
+//     }
+//     alpha = SUM / n;
+//     alphas.fill(1-alpha);
+//     hat_r.diag() = alphas;
+//     hat_R = hat_l.fill(alpha) + hat_r;
+//   }
   
   if (corr_str == "ar1")
   {
@@ -205,37 +211,37 @@ mat corr_est_normal_cpp(vec y_vect, mat x_mat, vec id_vect, vec beta_val, int n,
       SUM = SUM + sum(temp_resi.subvec(0, (m-2)) * temp_resi.subvec(1, (m-1))) / (m - 1);
     }
     alpha = SUM / n;
-    for (int j=0; j<m; j++ )
-    {
-      for (int k=0; k<m; k++)
-      {
-        hat_R(j,k) = pow(alpha,abs((k + 1) - (j + 1)));
-      }
-    }
+//     for (int j=0; j<m; j++ )
+//     {
+//       for (int k=0; k<m; k++)
+//       {
+//         hat_R(j,k) = pow(alpha,abs((k + 1) - (j + 1)));
+//       }
+//     }
   }
   
-  if (corr_str == "un")
-  {
-    resi_mat.insert_cols(0, resi);
-    resi_mat.reshape(m, n);
-    resi_mat = resi_mat.t();
-    hat_R = resi_mat.t() * resi_mat / n;
-    hat_R.diag() = ones;
-  }
-  return(hat_R);
+//   if (corr_str == "un")
+//   {
+//     resi_mat.insert_cols(0, resi);
+//     resi_mat.reshape(m, n);
+//     resi_mat = resi_mat.t();
+//     hat_R = resi_mat.t() * resi_mat / n;
+//     hat_R.diag() = ones;
+//   }
+  return(alpha);
 }
 
 
 // [[Rcpp::export]]
-List one_run_grid_cpp(vec y_vect, mat x_mat, vec id_vect, int n, int m, int pn, int fold, vec lam_vect, double rho, double eps, int eps_stop, int max_step, std::string corr_str){
+List one_run_grid_cpp(vec y_vect, mat x_mat, vec id_vect, int n, vec m, int pn, int fold, vec lam_vect, int eps_stop, std::string corr_str){
   mat hat_ini(m,m), hat_R_corr; hat_ini.eye();
   vec beta_ini(pn); beta_ini.fill(0);
   vec beta_shrink;
-  List beta_fit_indep = est_pgee_grid_cpp(y_vect, x_mat, id_vect, hat_ini, beta_ini, n, m, fold, lam_vect, eps, eps_stop, max_step);
+  List beta_fit_indep = est_pgee_grid_cpp(y_vect, x_mat, id_vect, hat_ini, beta_ini, n, m, fold, lam_vect, eps_stop);
   beta_shrink = as<vec>(beta_fit_indep[1]);
   //Estimate the correlation structure within subject and refit the model
   hat_R_corr = corr_est_normal_cpp(y_vect, x_mat, id_vect, beta_shrink, n, m, corr_str);
-  List beta_fit_corr = est_pgee_grid_cpp(y_vect, x_mat, id_vect, hat_R_corr, beta_ini, n, m, fold, lam_vect, eps, eps_stop, max_step);
+  List beta_fit_corr = est_pgee_grid_cpp(y_vect, x_mat, id_vect, hat_R_corr, beta_ini, n, m, fold, lam_vect, eps_stop);
   return List::create(
     Named("lam.cv.vector") = as<vec>(beta_fit_corr[0]),
     Named("beta.shrink.corr") = as<vec>(beta_fit_corr[1]),
